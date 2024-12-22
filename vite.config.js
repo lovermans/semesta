ea4c3f22c9cc30b430f113ae63a076c33d48ea40
lovermans <ilma.adzim@gmail.com>
2024-12-22 10:00:08 +0700

    Perbarui Konfigurasi Aplikasi

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import manifestSRI from 'vite-plugin-manifest-sri';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/theme.js'],
             refresh: true,
         }),
+        manifestSRI(),
     ],
 });
