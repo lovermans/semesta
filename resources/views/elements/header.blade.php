b1c07947f33bb7a5f6c5c49f9e3703ff70b55200
lovermans <ilma.adzim@gmail.com>
2025-01-29 16:21:09 +0700

    Theme Color Improvement

--- resources/views/elements/header.blade.php
+++ resources/views/elements/header.blade.php
@@ -8,7 +8,8 @@
 			</button>
 			<div class="title-header">
 				<a href="{{ $app->url->route('start') }}" title="{{ $app->config->get('app.name') }}">
-					<img alt="{{ $app->config->get('app.name') . ' App Icon' }}" src="{{ Vite::asset('resources/images/app-icon/app-icon-sprites.svg') . '#app-icon' }}">
+					<img alt="{{ $app->config->get('app.name') . ' App Icon' }}" class="svg"
+						src="{{ Vite::asset('resources/images/app-icon/app-icon-sprites.svg') . '#app-icon' }}">
 					{{ $app->config->get('app.name') }}
 				</a>
 			</div>
@@ -29,7 +30,7 @@
 				</svg>
 			</button>
 			<button popovertarget="user-navigation" title="User Menu">
-				<img alt="User Menu" aria-hidden="true" src="{{ Vite::asset('resources/images/svg/icon-sprites.svg') . '#account-circle-icon' }}">
+				<img alt="User Menu" aria-hidden="true" class="svg" src="{{ Vite::asset('resources/images/svg/icon-sprites.svg') . '#account-circle-icon' }}">
 			</button>
 			<a class="text-button" href="#login" title="Login">
 				Login
