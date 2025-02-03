4fe053ef8420814de5ee5b9fd9ffdb18b26b24e9
lovermans <ilma.adzim@gmail.com>
2025-02-03 16:42:53 +0700

    Dynamic Save and Change Color Theme

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -1,9 +1,15 @@
 
 function saveThemePreference() {
     // flip current value
-    theme.value = theme.value === 'light' ? 'dark' : 'light';
+    preference.theme = preference.theme === 'light' ? 'dark' : 'light';
 
     setThemePreference();
+};
+
+function saveColorPreference() {
+    preference.color = document.querySelector('#app-color-theme').value;
+
+    setColorPreference();
 };
 
 function getThemePreference() {
@@ -15,36 +21,84 @@ function getThemePreference() {
     }
 };
 
+function getColorPreference() {
+    if (localStorage.getItem('color-preference')) {
+        return localStorage.getItem('color-preference');
+    }
+    else {
+        return Math.floor(Math.random() * (1 + 360 - 0)) + 0;
+    }
+};
+
 function setThemePreference() {
-    localStorage.setItem('theme-preference', theme.value);
+    localStorage.setItem('theme-preference', preference.theme);
 
     reflectThemePreference();
 };
 
-function reflectThemePreference() {
-    document.firstElementChild.setAttribute('data-theme', theme.value);
+function setColorPreference() {
+    localStorage.setItem('color-preference', preference.color);
 
-    document.querySelector('#theme-toggle')?.setAttribute('aria-label', theme.value);
+    reflectColorPreference();
 };
 
-var theme = {
-    value: getThemePreference()
+function reflectThemePreference() {
+    document.firstElementChild.setAttribute('data-theme', preference.theme);
+
+    var themeToggle = document.getElementById('theme-toggle');
+
+    if (themeToggle) {
+        themeToggle.setAttribute('aria-label', preference.theme);
+    }
+};
+
+function reflectColorPreference() {
+    document.documentElement.style.setProperty('--hue', preference.color);
+
+    var colorSlider = document.getElementById('app-color-theme');
+
+    if (colorSlider) {
+        colorSlider.value = preference.color;
+    };
+};
+
+function updateColorPreference(hue) {
+    document.documentElement.style.setProperty('--hue', hue);
+};
+
+function shuffleColorPreference() {
+    localStorage.removeItem('color-preference');
+    preference.color = getColorPreference();
+
+    reflectColorPreference();
+};
+
+var preference = {
+    theme: getThemePreference(),
+    color: getColorPreference()
 };
 
 // set early so no page flashes / CSS is made aware
 reflectThemePreference();
+reflectColorPreference();
 
 window.addEventListener("DOMContentLoaded", () => {
     // set on load so screen readers can see latest value on the button
     reflectThemePreference();
+    reflectColorPreference();
 
     // now this script can find and listen for clicks on the control
-    document.querySelector('#theme-toggle')?.addEventListener('click', saveThemePreference);
+    document.getElementById('theme-toggle').addEventListener('click', saveThemePreference);
+    document.getElementById('save-app-color-theme').addEventListener('click', saveColorPreference);
+    document.getElementById('shuffle-app-color-theme').addEventListener('click', shuffleColorPreference);
+    document.getElementById('app-color-theme').addEventListener('input', (event) => {
+        updateColorPreference(event.target.value);
+    });
 });
 
 // sync with system changes
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', ({ matches: isDark }) => {
-    theme.value = isDark ? 'dark' : 'light';
+    preference.theme = isDark ? 'dark' : 'light';
 
     setThemePreference();
 });
--- resources/views/elements/navigation.blade.php
+++ resources/views/elements/navigation.blade.php
@@ -76,10 +76,10 @@
 
 	<div id="main-navigation-footer">
 		<div class="separator">
-			@lang('Theme Preference')
+			@lang('Theme')
 		</div>
 		<button aria-label="auto" aria-live="polite" class="header-button theme-toggle" id="theme-toggle">
-			@lang('Dark/Light Theme')
+			@lang('Dark/Light')
 			<svg aria-hidden="true" class="sun-and-moon" viewBox="0 0 24 24">
 				<mask class="moon" id="moon-mask">
 					<rect fill="white" height="100%" width="100%" x="0" y="0"></rect>
@@ -88,8 +88,26 @@
 				<use href="#theme-toggle-icon"></use>
 			</svg>
 		</button>
+		<label class="separator" for="app-color-theme">
+			@lang('Color')
+		</label>
+		<div id="app-color-slider">
+			<input id="app-color-theme" max="360" min="0" type="range" value="115">
+			<div id="app-color-actions">
+				<button id="shuffle-app-color-theme" title="@lang('Shuffle')">
+					<svg aria-hidden="true" class="menu-icon" viewbox="0 0 24 24">
+						<use href="#shuffle-icon"></use>
+					</svg>
+				</button>
+				<button id="save-app-color-theme" title="@lang('Save')">
+					<svg aria-hidden="true" class="menu-icon" viewbox="0 0 24 24">
+						<use href="#save-icon"></use>
+					</svg>
+				</button>
+			</div>
+		</div>
 		<label class="separator" for="app-locale">
-			@lang('Language Preference')
+			@lang('Language')
 		</label>
 		<form action="{{ $app->url->route('locale-setting') }}" class="language-selector" method="POST">
 			<select id="app-locale" name="locale">
